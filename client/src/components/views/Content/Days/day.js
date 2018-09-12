55876a4ac100fb4ffc766f189f4105056d93d320
Tran Trung Tinh <trantrungtinh4954@gmail.com>
2018-09-12 21:18:06 +0700

    dynamic day and handle click active day

--- client/src/components/views/Content/Days/day.js
+++ client/src/components/views/Content/Days/day.js
@@ -1,15 +1,38 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import * as moment from 'moment';
 
 export default class Day extends Component {
+
+  state = {
+    days: [0, 1, 2, 3, 4, 5, 6].map(num => moment().add(num, 'days')),
+    selected: moment()
+  }
+
+  formatDay = (day) => {
+    if(day.isSame(moment(), 'day')) return 'Today';
+    return day.format('ddd DD/MM');
+  }
+
+  isActive = (day) => {
+    return day.isSame(this.state.selected, 'day');
+  }
+
+  onSelected = (daySelected) => {
+    this.setState({ selected: daySelected });
+  }
+
   render() {
     return (
       <div className="day-select">
         <ul className="days">
-          <li className="day active">Today</li>
-          <li className="day">Monday</li>
-          <li className="day">11/09</li>
-          <li className="day">12/09</li>
-          <li className="day">13/09</li>
+          {
+            this.state.days.map((day, index) => 
+              <li 
+                key={index}
+                className={this.isActive(day) ? "day active" : "day"}
+                onClick={() => this.onSelected(day)}
+              >{this.formatDay(day)}</li>)
+          }
           <li className="day-selector">
             <span className="dec"></span>
             <span className="inc"></span>
